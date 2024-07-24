0b8ac5330bc5de4c4a8dbb7da834c596375c18e1
Chandler Abril <abrilchandler1@gmail.com>
2024-07-23 21:40:51 -0700

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color) VALUES ("Mr. Chocolate", 4, "M", "Brown");
+INSERT INTO bears (name, age, sex, color) VALUES ("Rowdy", 3, "M", "Black");
+INSERT INTO bears (name, age, sex, color) VALUES ("Tabitha", 5, "F", "Dark Brown");
+INSERT INTO bears (name, age, sex, color) VALUES ("Sergeant Brown", 1, "M", "Brown");
+INSERT INTO bears (name, age, sex, color) VALUES ("Melissa", 6, "F", "White");
+INSERT INTO bears (name, age, sex, color) VALUES ("Grinch", 8, "M", "Black");
+INSERT INTO bears (name, age, sex, color) VALUES ("Wendy", 4, "F", "Light Brown");
+INSERT INTO bears (name, age, sex, color) VALUES (NULL, NULL, NULL, NULL);
